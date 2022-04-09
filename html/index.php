2a8a7bd2d25e0db459f6588783f3d9384dc17ea8
mochiyuki55 <mochiyuki55@gmail.com>
2022-04-09 13:40:25 +0900

    環境構築

---
+++ html/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$msg = "How are you?";
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p> <?php echo $msg; ?> </p>
+</body>
+</html>
